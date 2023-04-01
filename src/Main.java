b9cb7bd7833372661288a967ed73b5a85cce19dd
admin <eeoneguifeed@gmail.com>
2023-04-01 18:59:56 +0600

    Task 3.

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,39 @@ public class Main {
         //int min = findMin(arr, arr.length);
         //System.out.println("Minimum is "+ min);
 
-        int[] arr = {3, 2, 4, 1};
-        double avg = findAvg(arr, arr.length);
-        System.out.println("Average is "+ avg/arr.length);
+        //2)
+        //int[] arr = {3, 2, 4, 1};
+        //double avg = findAvg(arr, arr.length);
+        //System.out.println("Average is "+ avg/arr.length);
+
+        int n = 10;
+        int check = n;
+        int count = 0;
+        System.out.println(checkComp(n,check,count));
     }
 
+    public static String checkComp(int n,int check, int count)
+    {
+        if(check != 0)
+        {
+            if(n % check == 0)
+            {
+                count += 1;
+            }
+        }
+        else if(check == 0)
+        {
+            if(count == 2)
+            {
+                return "Prime";
+            }
+            else
+            {
+                return "Composite";
+            }
+        }
+        return checkComp(n, check - 1, count);
+    }
     public static int findAvg(int arr[], int n)
     {
         if(n==1)
